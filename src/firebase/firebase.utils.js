7d83689e737bcbfac0305dd77b41bb6af48b5579
Perdikal <Alexis.perd@yahoo.com>
2020-08-11 01:57:51 +0300

    implemented firebase sign in created sign in page and added signIn with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBncs-yyPmNaQTTTaBBS8DPB3K0rdVmvPw",
+  authDomain: "perdikals-store-db.firebaseapp.com",
+  databaseURL: "https://perdikals-store-db.firebaseio.com",
+  projectId: "perdikals-store-db",
+  storageBucket: "perdikals-store-db.appspot.com",
+  messagingSenderId: "685945315446",
+  appId: "1:685945315446:web:ba196ba620c2cd6431ff57",
+  measurementId: "G-GC9SQRH6LT",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
